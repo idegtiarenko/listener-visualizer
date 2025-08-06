7c3e0ba15f5d9af4e6c29bb7962a807342cf815c
ievgen.degtiarenko <ievgen.degtiarenko@elastic.co>
2025-08-06 12:12:50 +0200

    convert to an action

--- src/main/kotlin/co/elastic/elasticsearch/listener/visualizer/ActionListenerPsiUtils.kt
+++ src/main/kotlin/co/elastic/elasticsearch/listener/visualizer/ActionListenerPsiUtils.kt
@@ -4,23 +4,32 @@ import com.intellij.psi.PsiClass
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiJavaCodeReferenceElement
 import com.intellij.psi.PsiLocalVariable
+import com.intellij.psi.PsiMethod
+import com.intellij.psi.PsiMethodCallExpression
 import com.intellij.psi.PsiParameter
 import com.intellij.psi.PsiReferenceExpression
 import com.intellij.psi.util.InheritanceUtil
 import com.intellij.psi.util.PsiTreeUtil
 
-object ListenerDetector {
+object ActionListenerPsiUtils {
 
-    fun isListener(element: PsiElement): Boolean {
-        // TODO register for leaf elements
+    fun signature(call: PsiMethodCallExpression): String {
+        if (call.methodExpression.reference == null) {
+            return "unknown"
+        }
+        val method = call.methodExpression.reference!!.resolve() as PsiMethod
+        return "${method.containingClass?.qualifiedName}:${method.name}"
+    }
+
+    fun isActionListener(element: PsiElement): Boolean {
         return when (element) {
-            is PsiParameter if isActionListener(PsiTreeUtil.findChildOfType(element, PsiJavaCodeReferenceElement::class.java)?.resolve()) -> true
-            is PsiReferenceExpression if isActionListener(element.resolve()) -> true
+            is PsiParameter -> isActionListenerType(PsiTreeUtil.findChildOfType(element, PsiJavaCodeReferenceElement::class.java)?.resolve())
+            is PsiReferenceExpression -> isActionListenerType(element.resolve())
             else -> false
         }
     }
 
-    private fun isActionListener(element: PsiElement?): Boolean {
+    private fun isActionListenerType(element: PsiElement?): Boolean {
         return when (element) {
             is PsiClass -> InheritanceUtil.isInheritor(element, "org.elasticsearch.action.ActionListener")
             is PsiLocalVariable -> InheritanceUtil.isInheritor(element.type, "org.elasticsearch.action.ActionListener")
