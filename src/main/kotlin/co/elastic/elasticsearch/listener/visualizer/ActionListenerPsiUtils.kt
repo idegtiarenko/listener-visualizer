1d92708e2ef83f05f1a2e5524efa0b1454845bb9
ievgen.degtiarenko <ievgen.degtiarenko@elastic.co>
2025-08-06 15:35:45 +0200

    detect listeners in fields

--- src/main/kotlin/co/elastic/elasticsearch/listener/visualizer/ActionListenerPsiUtils.kt
+++ src/main/kotlin/co/elastic/elasticsearch/listener/visualizer/ActionListenerPsiUtils.kt
@@ -1,15 +1,12 @@
 package co.elastic.elasticsearch.listener.visualizer
 
-import com.intellij.psi.PsiClass
 import com.intellij.psi.PsiElement
-import com.intellij.psi.PsiJavaCodeReferenceElement
+import com.intellij.psi.PsiField
 import com.intellij.psi.PsiLocalVariable
 import com.intellij.psi.PsiMethod
 import com.intellij.psi.PsiMethodCallExpression
 import com.intellij.psi.PsiParameter
-import com.intellij.psi.PsiReferenceExpression
 import com.intellij.psi.util.InheritanceUtil
-import com.intellij.psi.util.PsiTreeUtil
 
 object ActionListenerPsiUtils {
 
@@ -23,17 +20,9 @@ object ActionListenerPsiUtils {
 
     fun isActionListener(element: PsiElement): Boolean {
         return when (element) {
-            is PsiParameter -> isActionListenerType(PsiTreeUtil.findChildOfType(element, PsiJavaCodeReferenceElement::class.java)?.resolve())
-            is PsiReferenceExpression -> isActionListenerType(element.resolve())
-            else -> false
-        }
-    }
-
-    private fun isActionListenerType(element: PsiElement?): Boolean {
-        return when (element) {
-            is PsiClass -> InheritanceUtil.isInheritor(element, "org.elasticsearch.action.ActionListener")
-            is PsiLocalVariable -> InheritanceUtil.isInheritor(element.type, "org.elasticsearch.action.ActionListener")
             is PsiParameter -> InheritanceUtil.isInheritor(element.type, "org.elasticsearch.action.ActionListener")
+            is PsiLocalVariable -> InheritanceUtil.isInheritor(element.type, "org.elasticsearch.action.ActionListener")
+            is PsiField -> InheritanceUtil.isInheritor(element.type, "org.elasticsearch.action.ActionListener")
             else -> false
         }
     }
