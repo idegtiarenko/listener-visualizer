1be0e8a32ec32914162c4585c8259f3c52bacdbd
ievgen.degtiarenko <ievgen.degtiarenko@elastic.co>
2025-08-08 15:09:31 +0200

    convert to a tool window

--- src/main/kotlin/co/elastic/elasticsearch/listener/visualizer/AnalyzeActionListenerSpanAction.kt
+++ src/main/kotlin/co/elastic/elasticsearch/listener/visualizer/AnalyzeActionListenerSpanAction.kt
@@ -1,11 +1,17 @@
 package co.elastic.elasticsearch.listener.visualizer
 
+import co.elastic.elasticsearch.icons.Icons
 import co.elastic.elasticsearch.listener.visualizer.ActionListenerPsiUtils.isActionListener
-import com.intellij.openapi.actionSystem.ActionUpdateThread
-import com.intellij.openapi.actionSystem.ActionUpdateThreadAware
-import com.intellij.openapi.actionSystem.AnAction
-import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.actionSystem.*
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.wm.ToolWindow
+import com.intellij.openapi.wm.ToolWindowAnchor
+import com.intellij.openapi.wm.ToolWindowManager
+import com.intellij.ui.components.JBScrollPane
+import com.intellij.ui.content.Content
+import com.intellij.ui.content.ContentFactory
+import com.intellij.ui.content.ContentManager
+
 
 class AnalyzeActionListenerSpanAction: AnAction(), ActionUpdateThreadAware {
 
@@ -21,7 +27,26 @@ class AnalyzeActionListenerSpanAction: AnAction(), ActionUpdateThreadAware {
     override fun actionPerformed(e: AnActionEvent) {
         val element = e.getData(CommonDataKeys.PSI_ELEMENT)
         if (element != null && isActionListener(element)) {
-            ActionListenerSpanPopup(element).show()
+            val spans = JBScrollPane(ActionListenerSpanVisualizationBuilder.visualize(element))
+            val toolWindow = getOrCreateToolWindow("ActionListeners", element.project)
+            toolWindow.contentManager.addAndSelect(ContentFactory.getInstance().createContent(spans, element.text, false))
+            toolWindow.show()
         }
+    }
+
+    private fun getOrCreateToolWindow(id: String, project: Project): ToolWindow {
+        var toolWindow: ToolWindow? = ToolWindowManager.getInstance(project).getToolWindow(id)
+        if (toolWindow == null) {
+            toolWindow = ToolWindowManager.getInstance(project).registerToolWindow(id) {
+                icon = Icons.ES
+                anchor = ToolWindowAnchor.BOTTOM
+            }
+        }
+        return toolWindow
+    }
+
+    private fun ContentManager.addAndSelect(content: Content) {
+        addContent(content, 0)
+        setSelectedContent(content)
     }
 }
